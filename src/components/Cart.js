e802f8b332f3be9bddea5eb42cbf10199494bec5
Slawomir Andrzejczak <slawomir_andrzejczak@wp.pl>
2023-10-29 19:35:56 +0100

    Added cart and personal data components. Todo: creating order

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,11 +2,148 @@
 //Imports
 import React, {useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import {MDBBtn, MDBTable, MDBTableBody, MDBTableHead} from "mdb-react-ui-kit";
+import PersonalDataForm from "./PersonalData";
 
-export default function Cart() {
 
-    return (
-        <label htmlFor="categoryFilter">Filter by Category: </label>
-    );
+function switchProductIcon(category) {
+    //Check the category of product and return correct image
+    switch (category) {
+        case 1:
+            return <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS96mVnzQNDmWu65fo4LXL5jUx3qsWHTBFipz2evbD9Ww&s' alt='NotLoaded' style={{ width: '45px', height: '45px' }} className='rounded-circle' />;
+        case 2:
+            return <img src='https://cdn-icons-png.flaticon.com/512/7910/7910612.png' alt='NotLoaded' style={{ width: '45px', height: '45px' }} className='rounded-circle' />;
+        case 3:
+            return <img src='https://cdn-icons-png.flaticon.com/512/498/498266.png' alt='NotLoaded' style={{ width: '45px', height: '45px' }} className='rounded-circle' />;
+        case 4:
+            return <img src='https://icons.veryicon.com/png/o/food--drinks/fresh-1/meat-4.png' alt='NotLoaded' style={{ width: '45px', height: '45px' }} className='rounded-circle' />;
+        case 5:
+            return <img src='https://static.vecteezy.com/system/resources/previews/024/244/720/original/dairy-icon-in-illustration-vector.jpg' alt='NotLoaded' style={{ width: '45px', height: '45px' }} className='rounded-circle' />;
+        default:
+            return null;
+    }
 }
 
+export default function Cart() {
+    const [selectedProducts, setSelectedProducts] = useState(
+        JSON.parse(localStorage.getItem('myCart')) || []
+    );
+
+    const updateProductQuantity = (productId, newQuantity) => {
+        const updatedProducts = selectedProducts.map((product) =>
+            product._id === productId ? { ...product, quantity: newQuantity } : product
+        );
+        setSelectedProducts(updatedProducts);
+        localStorage.setItem('myCart', JSON.stringify(updatedProducts));
+    };
+
+    const handleIncreaseClick = (productId) => {
+        //Find current quantity of selected product and add 1
+        const updatedQuantity = selectedProducts.find((product) => product._id === productId).quantity + 1;
+        updateProductQuantity(productId, updatedQuantity);
+    };
+
+    const handleDecreaseClick = (productId) => {
+        // Find current quantity of selected product
+        const currentQuantity = selectedProducts.find((product) => product._id === productId).quantity;
+
+        if (currentQuantity > 1) {
+            // If quantity is greater than 1, decrease it
+            const updatedQuantity = currentQuantity - 1;
+            updateProductQuantity(productId, updatedQuantity);
+        } else {
+            // If quantity is 1, remove the product from the list
+            const updatedProducts = selectedProducts.filter((product) => product._id !== productId);
+            setSelectedProducts(updatedProducts);
+            localStorage.setItem('myCart', JSON.stringify(updatedProducts));
+        }
+    };
+
+    const removeProduct = (productId) => {
+        const updatedProducts = selectedProducts.filter((product) => product._id !== productId);
+        setSelectedProducts(updatedProducts);
+        localStorage.setItem('myCart', JSON.stringify(updatedProducts));
+    }
+
+    // Calculate total count and total cost
+    const totalCount = selectedProducts.reduce((total, product) => total + product.quantity, 0);
+    const totalCost = selectedProducts.reduce((total, product) => total + product.price * product.quantity, 0);
+
+
+    return (
+
+        <div>
+            <MDBTable align='middle'>
+                <MDBTableHead>
+                    <tr>
+                        <th scope='col'>Name</th>
+                        <th scope='col'>Action</th>
+                        <th scope='col'>Quantity</th>
+                        <th scope='col'>Cost</th>
+                        <th scope='col'>Total Price</th>
+                    </tr>
+                </MDBTableHead>
+                <MDBTableBody>
+                    {selectedProducts.map((property, index) => (
+                        <tr key={index}>
+                            <td>
+                                <div className='d-flex align-items-center'>
+                                    {switchProductIcon(property.category)}
+                                    <div className='ms-3'>
+                                        <p className='mb-1'>{property.name}</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='d-flex'>
+                                    <MDBBtn
+                                        color='success'
+                                        className='btn btn-default btn-work'
+                                        style={{ width: '50px', height: '30px' }}
+                                        onClick={() => handleIncreaseClick(property._id)}
+                                    >
+                                        +
+                                    </MDBBtn>
+                                    <MDBBtn
+                                        color='warning'
+                                        className='text-center'
+                                        style={{ width: '50px', height: '30px' }}
+                                        onClick={() => handleDecreaseClick(property._id)}
+                                    >
+                                        -
+                                    </MDBBtn>
+                                    <MDBBtn
+                                        color='danger'
+                                        className='text-center'
+                                        style={{ width: '50px', height: '30px' }}
+                                        onClick={() => removeProduct(property._id)}
+                                    >
+                                        X
+                                    </MDBBtn>
+                                </div>
+                            </td>
+                            <td>
+                                <p className='fw-normal mb-1'>{property.quantity}</p>
+                            </td>
+                            <td>
+                                <p className='fw-normal mb-1'>{property.price}</p>
+                            </td>
+                            <td>
+                                <p className='fw-normal mb-1'>{property.price * property.quantity}</p>
+                            </td>
+                        </tr>
+                    ))}
+                </MDBTableBody>
+                <tfoot>
+                <tr>
+                    <td>Total Count</td>
+                    <td>{totalCount}</td>
+                    <td>Total Cost</td>
+                    <td>{totalCost}</td>
+                </tr>
+                </tfoot>
+            </MDBTable>
+            <PersonalDataForm />
+        </div>
+    );
+}
---
+++ src/components/PersonalData.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBInput, MDBModal, MDBModalBody } from 'mdb-react-ui-kit';
+import Swal from "sweetalert2";
+
+const PersonalDataForm = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phoneNumber: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+        console.log('Form submitted:', formData);
+        // You can add further logic like sending the data to a server
+    };
+
+
+
+    const handleMakeOrderClick = () => {
+        let nameCheck = formData.name.length !== 0;  // true/false if length =0
+
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const phoneRegex = /(?<!\w)(\(?(\+|00)?48\)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}(?!\w)/;
+        // Test if the email matches the regex
+        let emailCheck = formData.email.length !== 0 && emailRegex.test(formData.email);  // true/false if length = 0, match regex
+        let phoneCheck = formData.phoneNumber.length !== 0 && phoneRegex.test(formData.phoneNumber);  // true/false if length = 0. match regex
+
+
+        let checks = nameCheck && emailCheck && phoneCheck;
+        // let ok;
+        if(checks)
+        {
+            Swal.fire({
+                icon: 'success',
+                title: 'Well done!',
+                text: 'Succesfully placed an order!',
+                footer: 'Confirmation'
+            })
+
+        }
+        else
+        {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'We\'ve encountered errors!\n' +
+                    '',
+                footer: 'Order validation error'
+            })
+        }
+    };
+
+    return (
+        <MDBContainer>
+            <MDBRow className="justify-content-center mt-5">
+                <MDBCol md="6">
+                    <form onSubmit={handleSubmit}>
+                        <MDBInput
+                            label="Your Name"
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                        <MDBInput
+                            label="Your Email"
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                        <MDBInput
+                            label="Your Phone Number"
+                            type="tel"
+                            id="phoneNumber"
+                            name="phoneNumber"
+                            value={formData.phoneNumber}
+                            onChange={handleChange}
+                        />
+                        <div className="text-center mt-4">
+                            <MDBBtn color="primary" type="submit"
+                                    style={{ width: '100px', height: '60px' }}
+                                    onClick={handleMakeOrderClick}>
+                                Make an order
+                            </MDBBtn>
+                        </div>
+                    </form>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
+    );
+};
+
+export default PersonalDataForm;
