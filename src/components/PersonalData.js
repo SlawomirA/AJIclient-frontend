fd8563fc87e02e6efebde70a26216c4974a06c48
AMalinowska03 <242463@edu.p.lodz.pl>
2023-11-13 13:11:08 +0100

    Added validation to personal data form

--- src/components/PersonalData.js
+++ src/components/PersonalData.js
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBInput, MDBModal, MDBModalBody } from "mdb-react-ui-kit";
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBInput,
+    MDBModal,
+    MDBModalBody,
+    MDBValidation,
+    MDBValidationItem,
+} from "mdb-react-ui-kit";
 import Swal from "sweetalert2";
 import { POST_ODRERS } from "../utils/Urls";
 import "../css/PersonalData.css";
@@ -11,13 +21,103 @@ const PersonalDataForm = () => {
         email: "",
         phoneNumber: "",
     });
+    const [feedbackName, setFeedbackName] = useState("");
+    const [feedbackEmail, setFeedbackEmail] = useState("");
+    const [feedbackPhone, setFeedbackPhone] = useState("");
 
     const handleChange = (e) => {
         const { name, value } = e.target;
+
         setFormData((prevData) => ({
             ...prevData,
             [name]: value,
         }));
+    };
+
+    const handleInput = (e) => {
+        console.log(e.target);
+        const { name, value } = e.target;
+        let element = document.getElementById(name);
+        if (name === "name") {
+            console.log("name: ", value.length);
+            console.log(e.target.parentElement);
+            console.log(element, element.classList);
+            if (value.length == 0) {
+                console.log("len 0");
+                if (!element.classList.contains("is-invalid")) {
+                    element.classList.add("is-invalid");
+                }
+                if (element.classList.contains("active")) {
+                    element.classList.remove("active");
+                }
+                setFeedbackName("Name cannot be empty");
+            } else if (value.length < 6) {
+                console.log("len<6");
+                if (!element.classList.contains("is-invalid")) {
+                    element.classList.add("is-invalid");
+                }
+                if (element.classList.contains("active")) {
+                    element.classList.remove("active");
+                }
+                setFeedbackName("Name must contain at least 6 characters");
+            } else {
+                if (element.classList.contains("is-invalid")) {
+                    element.classList.remove("is-invalid");
+                }
+                setFeedbackName("");
+            }
+        } else if (name === "email") {
+            console.log("email");
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if (value.length == 0) {
+                console.log("email len 0");
+                if (!element.classList.contains("is-invalid")) {
+                    element.classList.add("is-invalid");
+                }
+                if (element.classList.contains("active")) {
+                    element.classList.remove("active");
+                }
+                setFeedbackEmail("E-mail required");
+            } else if (emailRegex.test(value)) {
+                if (element.classList.contains("is-invalid")) {
+                    element.classList.remove("is-invalid");
+                }
+                setFeedbackEmail("");
+            } else {
+                if (!element.classList.contains("is-invalid")) {
+                    element.classList.add("is-invalid");
+                }
+                if (element.classList.contains("active")) {
+                    element.classList.remove("active");
+                }
+                setFeedbackEmail("E-mail not valid");
+            }
+        } else if (name === "phoneNumber") {
+            console.log("phone");
+            const phoneRegex = /(?<!\w)(\(?(\+|00)?48\)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}(?!\w)/;
+            if (value.length == 0) {
+                if (!element.classList.contains("is-invalid")) {
+                    element.classList.add("is-invalid");
+                }
+                if (element.classList.contains("active")) {
+                    element.classList.remove("active");
+                }
+                setFeedbackPhone("Phone number required");
+            } else if (phoneRegex.test(value)) {
+                if (element.classList.contains("is-invalid")) {
+                    element.classList.remove("is-invalid");
+                }
+                setFeedbackPhone("");
+            } else {
+                if (!element.classList.contains("is-invalid")) {
+                    element.classList.add("is-invalid");
+                }
+                if (element.classList.contains("active")) {
+                    element.classList.remove("active");
+                }
+                setFeedbackPhone("Phone number not valid");
+            }
+        }
     };
 
     const handleSubmit = (e) => {
@@ -114,54 +214,60 @@ const PersonalDataForm = () => {
         <MDBContainer>
             <div className="row">
                 <div className="col-md-12">
-                    <form onSubmit={handleSubmit} className="cart">
+                    <MDBValidation onSubmit={handleSubmit} className="cart" noValidate>
                         <div className="row form-row">
                             <div className="col-md-3">
-                                <label className="form-label" for="name">
-                                    Your name:
+                                <label className="form-label" htmlFor="name">
+                                    Your full name:
                                 </label>
                             </div>
-                            <div className="col-md-9">
+                            <MDBValidationItem className="col-md-9" feedback={feedbackName} invalid>
                                 <MDBInput
                                     type="text"
                                     id="name"
                                     name="name"
                                     value={formData.name}
                                     onChange={handleChange}
+                                    onInput={handleInput}
+                                    required
                                 />
-                            </div>
+                            </MDBValidationItem>
                         </div>
                         <div className="row form-row">
                             <div className="col-md-3">
-                                <label className="form-label" for="email">
+                                <label className="form-label" htmlFor="email">
                                     Your e-mail:
                                 </label>
                             </div>
-                            <div className="col-md-9">
+                            <MDBValidationItem className="col-md-9" feedback={feedbackEmail} invalid>
                                 <MDBInput
                                     type="email"
                                     id="email"
                                     name="email"
                                     value={formData.email}
                                     onChange={handleChange}
+                                    onInput={handleInput}
+                                    required
                                 />
-                            </div>
+                            </MDBValidationItem>
                         </div>
                         <div className="row form-row">
                             <div className="col-md-3">
-                                <label className="form-label" for="phoneNumber">
+                                <label className="form-label" htmlFor="phoneNumber">
                                     Your phone number:
                                 </label>
                             </div>
-                            <div className="col-md-9">
+                            <MDBValidationItem className="col-md-9" feedback={feedbackPhone} invalid>
                                 <MDBInput
                                     type="tel"
                                     id="phoneNumber"
                                     name="phoneNumber"
                                     value={formData.phoneNumber}
                                     onChange={handleChange}
+                                    onInput={handleInput}
+                                    required
                                 />
-                            </div>
+                            </MDBValidationItem>
                         </div>
 
                         <div className="row">
@@ -172,7 +278,7 @@ const PersonalDataForm = () => {
                                 </MDBBtn>
                             </div>
                         </div>
-                    </form>
+                    </MDBValidation>
                 </div>
             </div>
         </MDBContainer>
